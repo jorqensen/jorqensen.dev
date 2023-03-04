5b86f635c442c38a2fdd475b8c2872f696907f91
Mathias Jorgensen <mathias.jorqensen@gmail.com>
2023-03-05 00:47:08 +0100

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    site: 'https://jorqensen.dev',
+    integrations: [tailwind(), sitemap()]
 });
