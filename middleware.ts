f846a5a0ada5058ea282428330a72acfce7d37a9
Mathias Jorgensen <mathias.jorqensen@gmail.com>
2023-03-20 23:20:54 +0100

    fix user agent

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export default function middleware(request: Request) {
     const url = new URL(request.url);
     const agent = request.headers.get("User-Agent");
 
-    if (agent?.includes("Safari") && agent?.includes("Macintosh")) {
+    if ((agent?.includes("Safari") && agent?.includes("Macintosh")) || (agent?.includes("Safari") && agent?.includes("iPhone"))) {
         url.pathname = "/safari-user-agent";
         return Response.redirect(url.toString())
     }
