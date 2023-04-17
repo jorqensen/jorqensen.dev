dd632f0c6aff3b03f7d8e584623f00e254274666
Mathias Jorgensen <mathias.jorqensen@gmail.com>
2023-04-17 02:18:09 +0200

    design with components update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const plugin = require("tailwindcss/plugin")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		screens: {
-			'xs': '475px',
+			"xs": "475px",
 			...defaultTheme.screens,
 		},
 		extend: {
@@ -17,11 +18,22 @@ module.exports = {
 				}
 			},
 			fontFamily: {
-				rubik: ['RubikVariable', ...defaultTheme.fontFamily.sans],
+				rubik: ["RubikVariable", ...defaultTheme.fontFamily.sans],
+                "roboto-slab": ["Roboto SlabVariable", "Roboto Slab", ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
 	plugins: [
-        require('@tailwindcss/typography')
+        require("@tailwindcss/typography"),
+        plugin(function ({ addComponents }) {
+            addComponents({
+                ".bg-sun-tornado": {
+                    backgroundColor: "#09090b",
+                    backgroundImage: "url('/sun-tornado.svg')",
+                    backgroundAttachment: "fixed",
+                    backgroundSize: "cover",
+                }
+            })
+        })
     ],
 }
