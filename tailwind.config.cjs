da06e6856d838acd2112bdda370c3e0a6715f22a
Mathias Jorgensen <mathias.jorqensen@gmail.com>
2023-06-18 04:29:55 +0200

    tailwind config update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,34 +6,24 @@ const plugin = require("tailwindcss/plugin")
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		screens: {
-			"xs": "475px",
-			...defaultTheme.screens,
-		},
+        container: {
+            padding: "1.5rem",
+            center: true,
+        },
 		extend: {
 			colors: {
 				app: {
-					DEFAULT: colors.red[500],
-					...colors.red
+					DEFAULT: colors.amber[500],
+					...colors.amber
 				}
 			},
 			fontFamily: {
-				rubik: ["RubikVariable", ...defaultTheme.fontFamily.sans],
-                "roboto-slab": ["Roboto SlabVariable", "Roboto Slab", ...defaultTheme.fontFamily.sans],
+                inter: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+                lora: ['Lora Variable', ...defaultTheme.fontFamily.serif],
 			},
 		},
 	},
 	plugins: [
         require("@tailwindcss/typography"),
-        plugin(function ({ addComponents }) {
-            addComponents({
-                ".bg-sun-tornado": {
-                    backgroundColor: "#09090b",
-                    backgroundImage: "url('/sun-tornado.svg')",
-                    backgroundAttachment: "fixed",
-                    backgroundSize: "cover",
-                }
-            })
-        })
     ],
 }
